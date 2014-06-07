c738ea258b1fd78560434e535dc121cddc46bfcd
Jonathan Hoyt <hoyt@github.com>
2014-06-07 12:45:24 -0700

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,12 @@ current = Bucket.create(:name => "Current", :row_order => 0)
 # Create prioritized issues
 (99..117).each do |n|
   github_issue = Octokit.issue "bkeepers/dotenv", n
-
-  prioritized_issue = PrioritizedIssue.new :bucket => backlog
-  prioritized_issue.issue = Issue.create \
+  issue = Issue.create \
     :title             => github_issue["title"],
     :github_owner      => "bkeepers",
     :github_repository => "dotenv",
     :github_id         => n,
     :state             => github_issue["state"]
-  prioritized_issue.save
+
+  issue.prioritized_issues.create :bucket => icebox
 end
