0afbd2cb3a34ece9350b84aa4ae171a916256951
Jonathan Hoyt <hoyt@github.com>
2014-06-07 12:28:50 -0700

    Add Bucket#change_position

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,4 +1,24 @@
 class Bucket < ActiveRecord::Base
   include RankedModel
+
+  # Public: Bucket name.
+  # column :name
+  # Returns a String.
+  validates :name, :presence => true
+
+  # Public: Position of bucket.
+  # column :row_order
+  # Returns an Integer.
+  validates :row_order, :presence => true
   ranks :row_order
+
+  # Public: Change position of Bucket.
+  #
+  # position - Integer or symbol (:first, :last, :up, :down).
+  def change_position(position)
+    update_attribute :row_order_position, position
+  end
+
+  # column :created_at
+  # column :updated_at
 end
--- test/models/bucket_test.rb
+++ test/models/bucket_test.rb
@@ -2,10 +2,12 @@ require 'test_helper'
 
 class BucketTest < ActiveSupport::TestCase
   test "buckets are sortable" do
-    assert Bucket.find_by_name("Current").row_order < Bucket.find_by_name("Backlog").row_order
-    assert Bucket.find_by_name("Backlog").row_order < Bucket.find_by_name("Icebox").row_order
-    Bucket.find_by_name("Backlog").update_attribute :row_order, 0
-    assert Bucket.find_by_name("Backlog").row_order < Bucket.find_by_name("Current").row_order
-    assert Bucket.find_by_name("Current").row_order < Bucket.find_by_name("Icebox").row_order
+    current = buckets(:current)
+    backlog = buckets(:backlog)
+    icebox  = buckets(:icebox)
+    assert_equal [current, backlog, icebox], Bucket.rank(:row_order).all
+
+    current.change_position(:last)
+    assert_equal [backlog, icebox, current], Bucket.rank(:row_order).all
   end
 end
