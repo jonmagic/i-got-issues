0dd284f0de8baadbea3f2f465301e5317c3ccbf9
Jonathan Hoyt <hoyt@github.com>
2014-06-07 12:19:45 -0700

    Add ability to move around prioritized issues

---
+++ app/models/prioritized_issue.rb
@@ -0,0 +1,39 @@
+class PrioritizedIssue < ActiveRecord::Base
+  include RankedModel
+
+  # Public: Associated Issue.
+  # column :issue_id
+  validates :issue_id,  :presence => true
+  belongs_to :issue
+
+  # Public: Associated Bucket.
+  # column :bucket_id
+  validates :bucket_id, :presence => true
+  belongs_to :bucket
+
+  scope :bucket, ->(bucket) { where(:bucket => bucket).rank(:row_order) }
+
+  # Public: Priority of issue in bucket.
+  # column :row_order
+  validates :row_order, :presence => true
+  ranks :row_order, :with_same => :bucket_id
+
+  # Public: Change position of prioritized issue, the bigger the number the lower
+  # the priority (from a UI standpoint).
+  #
+  # position - Integer or symbol (:first, :last, :up, :down).
+  def change_position(position)
+    update_attribute :row_order_position, position
+  end
+
+  # Public: Move to bucket. Optionally position issue in bucket.
+  #
+  # bucket - Bucket instance.
+  # position - Integer or symbol (:first, :last, :up, :down).
+  def move_to_bucket(bucket, position = :last)
+    update_attributes :bucket => bucket, :row_order_position => position
+  end
+
+  # column :created_at
+  # column :updated_at
+end
---
+++ test/models/prioritized_issue_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class PrioritizedIssueTest < ActiveSupport::TestCase
+  test "reorder issues in a bucket" do
+    pi1 = prioritized_issues(:pi1)
+    pi3 = prioritized_issues(:pi3)
+    icebox = buckets(:icebox)
+
+    pi1.change_position(2)
+    assert_equal [pi3, pi1], PrioritizedIssue.bucket(icebox).all
+  end
+
+  test "move issue to different bucket and change priority" do
+    pi1 = prioritized_issues(:pi1)
+    pi2 = prioritized_issues(:pi2)
+    backlog = buckets(:backlog)
+
+    assert_equal "Icebox", pi1.bucket.name
+
+    pi1.move_to_bucket backlog, :first
+
+    assert_equal "Backlog", pi1.bucket.name
+    assert_equal [pi1, pi2], PrioritizedIssue.bucket(backlog).all
+  end
+end
