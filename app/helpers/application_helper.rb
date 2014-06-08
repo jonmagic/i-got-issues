b8fccd7ba983c3215eeee3abd9a2a59f1d546b17
Jonathan Hoyt <hoyt@github.com>
2014-06-08 01:43:22 -0700

    Link to issues on github

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def navbar_link_class(section)
     "active" if params[:controller] == section.to_s
   end
+
+  def github_issue_url(issue)
+    "https://github.com/#{issue.github_owner}/#{issue.github_repository}/issues/#{issue.github_id}"
+  end
 end
