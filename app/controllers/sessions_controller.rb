20019c8e9b76c7ef219f17d2f5e2f7c89c966a43
Jonathan Hoyt <hoyt@github.com>
2014-06-09 00:02:04 -0700

    Fix issue with trying to redirect and render

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,11 @@ class SessionsController < ApplicationController
   skip_before_filter :authenticate_user!, :only => :new
 
   def new
-    redirect_to buckets_path if current_user
-
-    render :layout => false
+    if current_user
+      redirect_to buckets_path
+    else
+      render :layout => false
+    end
   end
 
   def destroy
