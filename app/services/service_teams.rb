3b6b08e1f274faa1ea7f8d75db56ed6af92be0b5
Jonathan Hoyt <hoyt@github.com>
2014-07-18 21:21:16 -0700

    Rearrange methods

--- app/services/service_teams.rb
+++ app/services/service_teams.rb
@@ -8,17 +8,17 @@ module ServiceTeams
     end
   end
 
-  # Internal: Get team members for this team.
-  #
-  # Returns an Array of TeamMember instances.
-  def team_members
-    user.github_client.team_members(params[:team_id]).map {|team_params| TeamMember.new(team_params) }
-  end
-
   # Internal: The id for this team.
   #
   # Returns an Integer.
   def team_id
     params[:team_id]
+  end
+
+  # Internal: Get team members for this team.
+  #
+  # Returns an Array of TeamMember instances.
+  def team_members
+    user.github_client.team_members(params[:team_id]).map {|team_params| TeamMember.new(team_params) }
   end
 end
