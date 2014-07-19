0d8f506f15a78c9a0d5318fb579d13b738ebe493
Jonathan Hoyt <hoyt@github.com>
2014-07-18 21:36:26 -0700

    Refactor BucketUpdater

--- app/services/bucket_updater.rb
+++ app/services/bucket_updater.rb
@@ -12,12 +12,18 @@ class BucketUpdater
   def process
     authorize_write_team!
 
-    bucket = Bucket.find params[:id]
-    bucket.name               = name if name
+    bucket.name               = name     if name
     bucket.row_order_position = position if position
-    bucket.team_id            = team_id if team_id
+    bucket.team_id            = team_id  if team_id
     bucket.save
     bucket
+  end
+
+  # Internal: The Bucket that will be updated.
+  #
+  # Returns a Bucket.
+  def bucket
+    @bucket ||= Bucket.find(params[:id])
   end
 
   # Internal: Bucket name from params.
