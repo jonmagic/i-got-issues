a7a2c2d188979c55b1cabb23ffe812622402d0ce
Jonathan Hoyt <hoyt@github.com>
2014-07-26 22:21:51 -0700

    Change @team to team

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -5,11 +5,11 @@ class BucketsController < ApplicationController
 
   def index
     if team.buckets.any?
-      @buckets = @team.buckets
+      @buckets = team.buckets
       @columns = 12 / (@buckets.length > 0 ? @buckets.length : 1)
     elsif team.present?
       if team.member?(current_user)
-        redirect_to new_team_bucket_path(@team)
+        redirect_to new_team_bucket_path(team)
       else
         redirect_to teams_path
       end
@@ -27,11 +27,11 @@ class BucketsController < ApplicationController
 
   def create
     @bucket = Bucket.new(bucket_params)
-    @bucket.team_id = @team.id
+    @bucket.team_id = team.id
     @bucket.row_order_position = :last
 
     if @bucket.save
-      redirect_to team_path(@team), :notice => "Bucket was successfully created."
+      redirect_to team_path(team), :notice => "Bucket was successfully created."
     else
       render :new
     end
@@ -42,15 +42,15 @@ class BucketsController < ApplicationController
 
     respond_to do |format|
       format.json { render :json => @bucket }
-      format.html { redirect_to team_path(@team) }
+      format.html { redirect_to team_path(team) }
     end
   end
 
   def destroy
-    new_bucket = @team.buckets.where.not(:id => @bucket.id).last
+    new_bucket = team.buckets.where.not(:id => @bucket.id).last
     @bucket.issues.each {|issue| issue.move_to_bucket(new_bucket) }
     @bucket.destroy
-    redirect_to team_path(@team), :notice => "Bucket was successfully destroyed."
+    redirect_to team_path(team), :notice => "Bucket was successfully destroyed."
   end
 
   # Archives all closed, non-archived issues for the current user.
@@ -58,7 +58,7 @@ class BucketsController < ApplicationController
   # Note: In the future, when issues aren't limited to the user's team ID,
   # this will have to be scoped to the current team.
   def archive_closed_issues
-    @team.issues.closed.where(:archived_at => nil).update_all :archived_at => Time.now.beginning_of_minute
+    team.issues.closed.where(:archived_at => nil).update_all :archived_at => Time.now.beginning_of_minute
 
     head :ok
   end
@@ -66,7 +66,7 @@ class BucketsController < ApplicationController
 private
   # Use callbacks to share common setup or constraints between actions.
   def set_bucket
-    @bucket = @team.buckets.find(params[:id])
+    @bucket = team.buckets.find(params[:id])
   end
 
   # Only allow a trusted parameter "white list" through.
