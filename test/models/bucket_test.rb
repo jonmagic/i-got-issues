12b6ac63f56c2a5d5ae0b6b8d9bbf8e767dc1b90
Jonathan Hoyt <hoyt@github.com>
2014-08-05 18:09:21 -0700

    Add Bucket#current_position

--- test/models/bucket_test.rb
+++ test/models/bucket_test.rb
@@ -11,4 +11,10 @@ class BucketTest < ActiveSupport::TestCase
     current.change_position(:last)
     assert_equal [backlog, icebox, current], team.buckets.all
   end
+
+  test "#current_position returns column position" do
+    assert_equal 1, buckets(:current).current_position
+    assert_equal 2, buckets(:backlog).current_position
+    assert_equal 3, buckets(:icebox).current_position
+  end
 end
