6245ddd73b023f5de3d0e0a788a1158b23d4a991
Jonathan Hoyt <hoyt@github.com>
2014-08-05 21:28:17 -0700

    Add AuditEntriesController and audit log UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :teams, :only => :index do
     get  "/"                      => "buckets#index"
     post "/archive_closed_issues" => "teams#archive_closed_issues"
+    get  "/audit_log"            => "audit_entries#index"
 
     resources :buckets, :only => [:new, :edit, :create, :update, :destroy] do
       resources :prioritized_issues, :only => [:create, :update, :destroy] do
